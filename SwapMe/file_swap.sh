da4ef17460e8d264d491c1962c536d5a5c0170fc
Adam Clark <clark.adamm.m@gmail.com>
2025-09-26 23:30:03 -0400

    Update file_swap.sh
    Updated to add custom progress bar and add cleanup.sh

--- SwapMe/file_swap.sh
+++ SwapMe/file_swap.sh
@@ -1,3 +1,20 @@
-sh create_blank.sh
-sh move_files.sh
-sh organize_files.sh
+#!/bin/bash
+
+run_with_progress() {
+    local script=$1
+    local step_name=$2
+
+    echo "$step_name\r\c"
+    sh "$script" | while read line; do
+        echo "  $line"
+    done
+
+    echo "$step_name\r\c"
+
+
+}
+
+run_with_progress "create_blank.sh"   "Creating blank files |oooooooooooooooo| 0%"
+run_with_progress "move_files.sh"     "Moving files         |xxxxoooooooooooo| 25%"
+run_with_progress "organize_files.sh" "Organizing by type   |xxxxxxxxoooooooo| 50%"
+run_with_progress "cleanup.sh"        "Cleaning up          |xxxxxxxxxxxxoooo| 75%"
